1400548cfc9798af5fabc81fa44e1c836387b6c3
정우현 <teamidiots777@gmail.com>
2025-09-22 15:23:42 +0900

    [Bronze V] Title: 시험 성적, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/9498. 시험 성적/시험 성적.cc
@@ -0,0 +1,26 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int Grade = 0;
+    cin >> Grade;
+
+    if (Grade >= 90 && Grade <= 100) {
+        cout << "A";
+    }
+    else if (Grade >= 80 && Grade <= 89) {
+        cout << "B";
+    }
+    else if (Grade >= 70 && Grade <= 79) {
+        cout << "C";
+    }
+    else if (Grade >= 60 && Grade <= 69) {
+        cout << "D";
+    }
+    else {
+        cout << "F";
+    }
+
+    return 0;
+}

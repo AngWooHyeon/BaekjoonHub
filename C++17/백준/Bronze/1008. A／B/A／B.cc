09df8050f5caf4ec27bcfd7a129f9a13432869f1
정우현 <teamidiots777@gmail.com>
2025-09-22 14:39:45 +0900

    [Bronze V] Title: A/B, Time: 0 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/1008. A／B/A／B.cc
@@ -0,0 +1,10 @@
+#include <iostream>
+#include <iomanip>
+using namespace std;
+
+int main() {
+    int A, B;
+    cin >> A >> B;
+    cout << fixed << setprecision(10) << (double)A / B;
+    return 0;
+}

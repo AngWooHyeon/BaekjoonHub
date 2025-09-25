2f21f99197d09388ce3c3fe242e7d15fcb71acde
정우현 <teamidiots777@gmail.com>
2025-09-25 20:34:06 +0900

    [Bronze V] Title: A+B - 3, Time: 4 ms, Memory: 2020 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/10950. A＋B － 3/A＋B － 3.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int T,A,B = 0;
+    cin >> T;
+
+    for (int i = 1; i <= T; i++)
+    {
+        cin >> A >> B;
+        cout << A+B << endl;
+    }
+
+    return 0;
+}

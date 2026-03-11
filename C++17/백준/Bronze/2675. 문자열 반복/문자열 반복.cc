0cbe5de842616ed99f1c06c6138f2cb592c721fc
정우현 <teamidiots777@gmail.com>
2026-03-11 19:29:05 +0900

    [Bronze II] Title: 문자열 반복, Time: 0 ms, Memory: 2024 KB -BaekjoonHub

---
+++ C++17/백준/Bronze/2675. 문자열 반복/문자열 반복.cc
@@ -0,0 +1,23 @@
+#include <iostream>
+#include <string>
+using namespace std;
+
+int main() {
+    int T;
+    cin >> T;
+
+    while (T--) {
+        int R;
+        string S;
+        cin >> R >> S;
+
+        for (char c : S) {
+            for (int i = 0; i < R; i++) {
+                cout << c;
+            }
+        }
+        cout << '\n';
+    }
+
+    return 0;
+}
